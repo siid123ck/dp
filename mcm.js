5a9facd29d71ef9e7d5ed809a0a2f090b5b34aab
siid sharma <siid123ck@gmail.com>
2021-09-20 11:39:56 +1000

    mcm tabulation

--- mcm.js
+++ mcm.js
@@ -1,5 +1,5 @@
 function mcm(arr, left=1, right=arr.length-1){
-    if(left===right) return 0; 
+    if(left >= right) return 0; 
     let max = Infinity, temp; 
     for(let k=left; k<right; k++){
         temp = mcm(arr, left, k) + mcm(arr, k+1, right) + (arr[left-1] * arr[k] * arr[right]);
@@ -9,5 +9,30 @@ function mcm(arr, left=1, right=arr.length-1){
     return max;
 }
 
-let result = mcm([10, 20, 5]);
+function mcmTab(arr, n=arr.length){
+    let table= Array(n).fill(0).map(()=>Array(n).fill(Infinity))
+    let right, cost;
+    if(n===0) return 0;
+    for(let i=0; i<n; i++){
+        table[i][i] = 0
+    }
+    
+    for(let length=2; length<n; length++){
+        for(let left = 1; left < n-length + 1; left++){
+            right = length + left -1; 
+            if(left===right) table[left][right] = 0;
+
+            // table[left][right] = Infinity;
+            for(let k=left; k<right; k++){
+            cost = table[left][k] + table[k+1][right] + arr[left-1]*arr[k]*arr[right];
+            table[left][right] = Math.min(cost, table[left][right])
+            }
+        }
+    }
+
+    console.log(table)
+    return table[1][n-1]
+}
+
+let result = mcm([10, 40, 50, 60]);
 console.log(result)
